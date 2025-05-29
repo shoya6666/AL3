c75145be0946d40a63edd6cd72dc6fc6b0c8647c
shoya6666 <shoya061666@icloud.com>
2025-05-29 14:26:42 +0900

    a

--- DirectXGame/Mymath.cpp
+++ DirectXGame/Mymath.cpp
@@ -2,6 +2,7 @@
 using namespace KamataEngine;
 using namespace MathUtility;
 
+
 KamataEngine::Matrix4x4 MakeAffineMatrix(KamataEngine::Vector3& scale, KamataEngine::Vector3& rotation, KamataEngine::Vector3& translation) {
 
 	Matrix4x4 matScale = MakeScaleMatrix(scale);
